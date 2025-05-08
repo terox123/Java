b0440d54d1c41ed968675001e61a963bbb50906e
Bulat <okhotnikov_99@bk.ru>
2025-05-08 21:49:55 +0300

    function add

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,8 +1,13 @@
-import java.awt.image.PackedColorModel;
+import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        System.out.println(add(1, 3));
+    }
+
+    public static int add(int a, int b){
+return a + b;
     }
 
 }
