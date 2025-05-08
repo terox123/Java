cf1865c50d45cd9b4974ba9e7af3534d75b54c33
Bulat <okhotnikov_99@bk.ru>
2025-05-08 21:53:23 +0300

    function sub

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,13 +1,15 @@
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
 
         System.out.println(add(1, 3));
+        System.out.println(sub(3, 2));
+
     }
 
     public static int add(int a, int b){
 return a + b;
     }
-
+public static int sub(int a, int b){
+    return a - b;
+    }
 }
